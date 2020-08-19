d0fd21a3fb567381353914bc3db046845ccd3165
Vakar <39268336+Vakar13@users.noreply.github.com>
2020-08-19 13:16:17 +0300

    Update my.js

--- my.js
+++ my.js
@@ -11,4 +11,25 @@ fetch(url)
     })
 }
 
+function addGS(){
+    
+    
+    const url = "https://script.google.com/macros/s/AKfycbyzl35T-a2CSjtynZ3UtCMw2Xc_Xog7kO6auI5Q1YP26TFLfQ/exec"
+    
+    
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name:"John"}) // body data type must match "Content-Type" header
+      });
+        
+    }
+document.getElementById("btn2").addEventListener("click",addGS);
+
 document.getElementById("btn").addEventListener("click",testGS);
